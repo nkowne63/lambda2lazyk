678fc2ae9c664e536767e2986b9204e77bef0be0
nkowne63rt <neutron63zf@gmail.com>
2023-11-17 02:49:56 +0900

    fix: intoLazy

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,12 @@ data LambdaTerm a
   | Abs a (LambdaTerm a)
   deriving (Show, Eq)
 
+isFreeAppearInLambdaTerm :: (Eq a) => LambdaTerm a -> a -> Bool
+isFreeAppearInLambdaTerm (Var x) y = x == y
+isFreeAppearInLambdaTerm (App e1 e2) x = isFreeAppearInLambdaTerm e1 x || isFreeAppearInLambdaTerm e2 x
+isFreeAppearInLambdaTerm (Abs x e) y = x /= y && isFreeAppearInLambdaTerm e y
+
+-- Evaluation用の中間状態
 data LazyTermEv a
   = EvC (LazyTermEv a) (LazyTermEv a)
   | EvS
@@ -22,9 +28,17 @@ data LazyTermEv a
 intoLazy :: (Eq a) => LambdaTerm a -> LazyTermEv a
 intoLazy (Var x) = EvVar x
 intoLazy (App e1 e2) = EvC (intoLazy e1) (intoLazy e2)
-intoLazy (Abs x (App e1 e2)) = EvC (EvC EvS (intoLazy (Abs x e1))) (intoLazy (Abs x e2))
-intoLazy (Abs x (Abs y e)) = EvAbs x (intoLazy (Abs y e))
-intoLazy (Abs x e) = if e == Var x then EvI else EvC EvK (intoLazy e)
+intoLazy (Abs x e)
+  | not (isFreeAppearInLambdaTerm e x) = EvC EvK (intoLazy e)
+  | e == Var x = EvI
+  | otherwise =
+      case e of
+        App e1 e2 ->
+          EvC
+            (EvC EvS (intoLazy (Abs x e1)))
+            (intoLazy (Abs x e2))
+        Abs y e1 -> EvAbs x (intoLazy (Abs y e1))
+        _ -> error "impossible"
 
 isFreeAppearInLazyTermEv :: (Eq a) => LazyTermEv a -> a -> Bool
 isFreeAppearInLazyTermEv EvS _ = False
